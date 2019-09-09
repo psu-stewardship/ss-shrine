78a20c1a65207a39e974f5c4632e734163318fab
Adam Wead <awead@users.noreply.github.com>
2019-09-09 16:03:11 -0400

    Create status endpoint to monitor async events (#4)
    Uses Stimulus.js to poll an endpoint where we inquire about the status
    of a post's async processing.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@
 
 class Post < ApplicationRecord
   include ImageUploader::Attachment(:file)
+
+  def json_file_data
+    HashWithIndifferentAccess.new(JSON.parse(file_data))
+  end
 end
---
+++ app/views/posts/status.json.jbuilder
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+json.promoted @status.promoted?
+json.derived @status.derived?
