0cf03f642e9b2381a6b3ccde5155dc96d4d91043
Dann Bohn <djb44@psu.edu>
2019-09-08 20:08:28 -0400

    presign endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@
 Rails.application.routes.draw do
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Shrine.presign_endpoint(:cache) => "/s3/params"
+  
 end
