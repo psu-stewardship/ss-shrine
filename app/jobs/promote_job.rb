076738805456a5c53549b3f1e4de33f741bd5189
Dann Bohn <djb44@psu.edu>
2019-09-09 16:24:07 -0400

    remove sleep

--- app/jobs/promote_job.rb
+++ app/jobs/promote_job.rb
@@ -5,10 +5,6 @@ class PromoteJob < ApplicationJob
 
   def perform(record:, name:, file_data:)
     attacher = Shrine::Attacher.retrieve(model: record, name: name.to_sym, file: file_data)
-    10.times do
-      Rails.logger.warn("I'm resting...")
-      sleep(1)
-    end
     record.file_derivatives!(:thumbnails)
     attacher.atomic_promote
   rescue Shrine::AttachmentChanged, ActiveRecord::RecordNotFound
