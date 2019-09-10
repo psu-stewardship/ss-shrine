3263c134029d0584639170f24fd3c5ac3b8c2041
Dann Bohn <djb44@psu.edu>
2019-09-09 20:33:01 -0400

    sidekiq initializer

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,12 @@
+redis_config = Rails.application.config_for(:redis)
+
+url = "redis://#{redis_config.fetch('host', 'localhost')}:#{redis_config.fetch('port', 6379)}"
+redis_config['url'] = url
+
+Sidekiq.configure_server do |config|
+  config.redis = redis_config
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = redis_config
+end
