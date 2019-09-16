1f41227a0449b7dcc8480ac02a5b370f1d37b967
Adam Wead <amsterdamos@gmail.com>
2019-09-16 11:38:12 -0400

    Don't run derivatives during promotion

--- app/jobs/promote_job.rb
+++ app/jobs/promote_job.rb
@@ -5,7 +5,6 @@ class PromoteJob < ApplicationJob
 
   def perform(record:, name:, file_data:)
     attacher = Shrine::Attacher.retrieve(model: record, name: name.to_sym, file: file_data)
-    record.file_derivatives!(:derivatives)
     attacher.atomic_promote
   rescue Shrine::AttachmentChanged, ActiveRecord::RecordNotFound
   end
