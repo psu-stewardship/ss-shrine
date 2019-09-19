297931ca57b6766ff3a343fb3ae01124f0c4841c
Adam Wead <amsterdamos@gmail.com>
2019-09-19 10:04:30 -0400

    Display names and better download links for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,14 @@ class Post < ApplicationRecord
   def file_data
     HashWithIndifferentAccess.new(super)
   end
+
+  def display_name
+    title || file_data.fetch(:metadata, {}).fetch(:filename, '[na]')
+  end
+
+  private
+
+    def filename
+      @filename ||= file_data.fetch(:metadata, {}).fetch(:filename, '[na]')
+    end
 end
